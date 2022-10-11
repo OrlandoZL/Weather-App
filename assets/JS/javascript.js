d45b72e5a44768e66386868354ac348c998e8744
Orlando <orlandozepeda20@gmail.com>
2022-10-10 22:50:02 -0700

    added current day and future weather var's

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -7,7 +7,7 @@ function currentWeather(city) {
 
     $.ajax({
         url: queryURL,
-        method: "get"
+        method: "GET"
     })
     .then(function(cityWeatherResponse){
         console.log(cityWeatherResponse);
@@ -22,6 +22,47 @@ function currentWeather(city) {
         <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
         <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
         <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
-        `)
-    })
-}
+        `);
+    
+        $("#cityDetail").append(currentCity);
+
+        var lat = cityWeatherResponse.coord.lat;
+        var lon = cityWeatherResponse.coord.lon;
+        var uviQueryURL = 'https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}';
+
+        $.ajax({
+            url: uviQueryURL,
+            method: "GET"
+        })
+        .then(function(uviResponse){
+            console.log(uviResponse);
+
+            var uvIndex = uviResponse.value;
+        })
+    })}
+
+    function futureCondition(lat, lon) {
+
+        var futureURL = "https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}";
+
+        $.ajax({
+            url: futureURL,
+            method: "GET"
+        })
+        .then(function(futureResponse){
+            console.log(futureResponse);
+            $("#fiveDay").empty();
+
+            for (let i = 1; i < 6; i++) {
+                var cityInfo = {
+                    date: futureResponse.daily[i].dt,
+                    icon: futureResponse.daily[i].weather[0].icon,
+                    temp: futureResponse.daily[i].temp.day,
+                    humidity: futureResponse.daily[i].humidity
+                };
+                 
+                var currDate = moment.unix(cityInfo.date).format("MM//DD/YYYY");
+                var iconURL = '<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png"/>'
+            }
+        })
+    }
